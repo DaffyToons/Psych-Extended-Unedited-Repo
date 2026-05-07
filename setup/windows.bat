653444ea200ef9c119313c3158ea79a223cd6b9c
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2026-05-07 17:50:44 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -13,11 +13,11 @@ haxelib git hscript-improved https://github.com/PsychExtendedThings/hscript-impr
 haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet
-haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet
+haxelib git hxcpp https://github.com/NovaFlare-Engine-haxelib/hxcpp-4.3.2-legacy --quiet
 haxelib git lime https://github.com/ArkoseLabsOfficial/lime --quiet
 haxelib install openfl 9.3.3 --quiet
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
-haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
+haxelib git funkin.vis https://github.com/NovaFlare-Engine-haxelib/funkVis-FrequencyFixed main --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git 57f5d47f2533fd0c3dcd025a86cb86c0dfa0b6d2 --quiet
 echo Finished!
 pause
