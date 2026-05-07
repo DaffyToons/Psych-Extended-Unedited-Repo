b5099ac202cac818f37643989f219c79d2757a70
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2026-05-07 17:49:31 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -16,10 +16,10 @@ haxelib git hscript-improved https://github.com/PsychExtendedThings/hscript-impr
 haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib git hxCodec https://github.com/PsychExtendedThings/hxCodec-0.6.3 --quiet #That clearly my repo
-haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
+haxelib git hxcpp https://github.com/NovaFlare-Engine-haxelib/hxcpp-4.3.2-legacy --quiet #Just a normal hxcpp
 haxelib git lime https://github.com/ArkoseLabsOfficial/lime-termux --quiet #repo owner helped to fix termux problem
 haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
-haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
+haxelib git funkin.vis https://github.com/NovaFlare-Engine-haxelib/funkVis-FrequencyFixed main --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git 57f5d47f2533fd0c3dcd025a86cb86c0dfa0b6d2 --quiet
 echo Finished!
